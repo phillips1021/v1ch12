7d058742a100b5dc8314ded3dc7602edaaa5f95c
brucephillips <bphillips@ku.edu>
2018-12-08 16:29:07 -0600

    code cleanup

--- blockingQueue/BlockingQueueTest.java
+++ blockingQueue/BlockingQueueTest.java
@@ -115,7 +115,7 @@ public class BlockingQueueTest
                catch (InterruptedException e)
                {
                }
-               
+
             };
 
             new Thread(searcher).start();
